df225e66257ea4db253f049d06e1536955312920
Joinal Abden <joinalzx@gmail.com>
2025-07-30 15:14:32 +0600

    basic project done

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let firstName = "Joinal";
+let lastName = "Abden";
+let village = "Chandpur, chariani, Nasircoat"
+
+let age = 38
+age = 37
+
+const num = 23
+num = 24
+console.log(num);
+
+console.log('My name is '+ firstName + ' ' + lastName);
+console.log("I am "+ age);
+console.log("I live in "+ village);
+
+
