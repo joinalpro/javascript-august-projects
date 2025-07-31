9e2ebfeb80d0be9a867cbb80fe10301316d08c26
Joinal Abden <joinalzx@gmail.com>
2025-07-31 19:20:43 +0600

    for loop

--- project-work/main.js
+++ project-work/main.js
@@ -4,16 +4,50 @@
 
 // document.getElementById('greeting').innerHTML = greeting + firstName
 
-let friends = ['Alan', 'Mark', 'Andy', 'Scott']
-console.log(friends);
+// let friends = ['Alan', 'Mark', 'Andy', 'Scott']
+// console.log(friends);
 
 
-friends [1] = 'Anna'
-friends [3] = 'Abden'
-friends [4] = 'Maverick'
-friends [5] = 1
+// friends [1] = 'Anna'
+// friends [3] = 'Abden'
+// friends [4] = 'Maverick'
+// friends [5] = 1
 
-console.log(friends.indexOf('Scott'));
+// console.log(friends.indexOf('Scott'));
 
-console.log(friends);
-console.log(friends[1]);
+// console.log(friends);
+// console.log(friends[1]);
+
+// let age = 10
+
+// if (age >= 18){
+//     console.log("you are an adult");
+// }
+// else {
+//     console.log("you are not adult");
+// }
+
+// let grade = parseInt(prompt("Please enter your grade 0 - 100: "));
+// console.log(typeof grade);
+// if (grade >= 90){
+//     alert ("You grade is A.");
+// }
+// else if (grade >= 80 ){
+//     alert ("you grade is B.")
+// }
+// else if (grade >= 70 ){
+//     alert ("you grade is C.")
+// }
+// else if (grade >= 60) {
+//     alert ("You grade is D.")
+// }
+// else {
+//     console.log("You grade is F");
+// }
+
+
+// For loop
+
+for (let i = 1; i<= 5; i++){
+    console.log("Hi " + i);
+}
