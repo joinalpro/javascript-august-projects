b4a45c209bb4c927300fc2ec3d924d92130201d2
Joinal Abden <joinalzx@gmail.com>
2025-07-31 15:05:35 +0600

    project 6 done

---
+++ project-work/main.js
@@ -0,0 +1,19 @@
+// let firstName = prompt("Please enter your first name: ");
+
+// let greeting = "Hello "
+
+// document.getElementById('greeting').innerHTML = greeting + firstName
+
+let friends = ['Alan', 'Mark', 'Andy', 'Scott']
+console.log(friends);
+
+
+friends [1] = 'Anna'
+friends [3] = 'Abden'
+friends [4] = 'Maverick'
+friends [5] = 1
+
+console.log(friends.indexOf('Scott'));
+
+console.log(friends);
+console.log(friends[1]);
